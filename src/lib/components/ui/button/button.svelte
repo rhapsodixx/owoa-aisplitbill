6310ea36612c6c6210c7140173841ca7b33ecc7a
Panji Gautama <panji.gautama@tiptip.tv>
2026-01-11 20:07:32 +0700

    feat(ui): add input, textarea, label, card, and button components

---
+++ src/lib/components/ui/button/button.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { type ButtonVariant, buttonVariants } from "./button-variants.js";
+  import { cn } from "$lib/utils";
+
+  export let variant: ButtonVariant["variant"] = "default";
+  export let size: ButtonVariant["size"] = "default";
+  export let className: string | undefined = undefined;
+  export let href: string | undefined = undefined;
+  export let type: "button" | "submit" | "reset" = "button";
+  export let disabled: boolean = false;
+</script>
+
+<svelte:element
+  this={href ? "a" : "button"}
+  {href}
+  {type}
+  {disabled}
+  class={cn(buttonVariants({ variant, size, className }))}
+  {...$$restProps}
+  on:click
+  on:submit
+>
+  <slot />
+</svelte:element>
---
+++ src/lib/components/ui/button/index.ts
@@ -0,0 +1,9 @@
+import Root from "./button.svelte";
+import { buttonVariants } from "./button-variants.js";
+
+export {
+	Root,
+	//
+	Root as Button,
+	buttonVariants,
+};
